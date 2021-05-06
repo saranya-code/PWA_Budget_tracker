2fc40bdde10e98c99a1feed1d63540f17123073c
saranya-code <saranyadayalan28@gmail.com>
2021-05-06 14:39:59 -0500

    updataed in manifest and mongoURI

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # PWA_Budget_tracker
 Budget Tracker application to allow for offline access and functionality.
+
+## Description
+* The user will be able to add expenses and deposits to their budget with or without a connection.
+* When the user inputs a withdrawal or deposit transactions(deposits,expenses) offline, they should 
+  populate the total when brought back online.
+* Adding offline entries to the transaction history, it should show on the webpage when connection is 
+  back online.
+
+## Installation
+
+  npm i
+
+## Dependencies
+
+* compression
+* express
+* mongoose
+* morgan
+* lite-server
+
+## Usage
+
+After cloning the repo,use the command line to navigate to the root of the application and run the command 'node server.js' to start the server.
+
+## Lisence
+
+This program is licensed under the ISC license.
+
+## screenshot 
+
+![Webpage Screenshot](./images/.png?raw=true) 
+
+# Resource links
+
+Heroku Deployment URL : https://afternoon-gorge-85233.herokuapp.com/
+
---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Perform install steps
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+//Fetch
+self.addEventListener("fetch", function(event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, we will store in the cache 
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+
+          
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
